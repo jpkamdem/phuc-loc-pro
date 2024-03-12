fc20c11ac10e64b7c22f1b118c9a39ae344a78a5
jp kam laptop <jpkamdem@proton.me>
2024-03-12 13:28:19 +0100

    premier commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { AccueilComponent } from './views/accueil/accueil.component';
+import { InfosComponent } from './views/infos/infos.component';
+import { NotfoundComponent } from './views/notfound/notfound.component';
+import { PanierComponent } from './views/panier/panier.component';
+import { CarteComponent } from './views/carte/carte.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: AccueilComponent},
+    {path: 'accueil', component: AccueilComponent},
+    {path: 'infos', component: InfosComponent},
+    {path: 'notfound', component: NotfoundComponent},
+    {path: 'panier', component: PanierComponent},
+    {path: 'carte', component: CarteComponent},
+    {path: '**', component: NotfoundComponent}
+];
---
+++ src/app/views/carte/carte.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'carte',
+  standalone: true,
+  imports: [],
+  templateUrl: './carte.component.html',
+  styleUrl: './carte.component.css'
+})
+export class CarteComponent {
+
+}
