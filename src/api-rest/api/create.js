02586aea2c609cf9408fa21d498fd96ed5a97aaf
jpkamdem <jpkamdem@proton.me>
2024-03-15 01:43:45 +0100

    sql 'avancé'

--- src/api-rest/api/create.js
+++ src/api-rest/api/create.js
@@ -1,19 +1,23 @@
 const connection  = require("../config/database.js")
 
-connection.query("INSERT INTO Aliments (nom) VALUES ('test255')", (err) => {
-  if (err) {
-    console.log("Erreur dans la requête d'insertion", err)
-    return;
-  }
-  console.log("Requête d'insertion effectuée")
+// connection.query("INSERT INTO Aliments (nom) VALUES ('test255')", (err) => {
+//   if (err) {
+//     console.log("Erreur dans la requête d'insertion", err)
+//     return;
+//   }
+//   console.log("Requête d'insertion effectuée")
 
-  connection.query("SELECT * FROM Aliments", (err, res) => {
-    if (err) {
-      console.log("Erreur dans la requête de lecture")
-      return;
-    }
-    console.log("Résultat de la requête de lecture : ", res)
+//   connection.query("SELECT * FROM Aliments", (err, res) => {
+//     if (err) {
+//       console.log("Erreur dans la requête de lecture")
+//       return;
+//     }
+//     console.log("Résultat de la requête de lecture : ", res)
 
-    connection.end()
-  })
-})
+//     connection.end()
+//   })
+// })
+
+let table = ''
+let value = ''
+connection.query(`insert into ${table}` )
